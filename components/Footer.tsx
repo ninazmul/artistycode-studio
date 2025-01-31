535b137abdcda607902d61405ed3708842f29540
ninazmul <nazmulsaw@gmail.com>
2025-01-31 12:16:04 +0600

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -50,7 +50,7 @@ const Footer = () => {
           achieve your goals.
         </p>
         <div className="flex flex-wrap justify-center items-center gap-5">
-          <a href="mailto:artistycodestudio@gmail.com">
+          <a href="mailto:artistycodestudio@gmail.com" target="_blank">
             <MagicButton
               title="Contact via Email"
               icon={<FaEnvelope />}
