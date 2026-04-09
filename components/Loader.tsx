b10190efc48db186950652ea9c4dd7e45099c626
ninazmul <nazmulsaw@gmail.com>
2026-04-09 12:02:34 +0600

    Loader text updated

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,8 +4,8 @@ const Loader = (props: Props) => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="loader">
-        <span>ARTISTYCODESTUDIO</span>
-        <span>ARTISTYCODESTUDIO</span>
+        <span>ARTISTYCODE STUDIO</span>
+        <span>ARTISTYCODE STUDIO</span>
       </div>
     </div>
   );
