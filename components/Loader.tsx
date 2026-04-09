8eaed62cc11e3950539b8c1acd5500a6da0c0e9e
ninazmul <nazmulsaw@gmail.com>
2026-04-09 11:59:58 +0600

    Loader ui updated and made more premium

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,17 +3,6 @@ type Props = {};
 const Loader = (props: Props) => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      {/* <div className="loader"></div> */}
-      {/* <Watch
-        visible={true}
-        height="80"
-        width="80"
-        radius="48"
-        color="#19cec6"
-        ariaLabel="watch-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      /> */}
       <div className="loader">
         <span>ARTISTYCODESTUDIO</span>
         <span>ARTISTYCODESTUDIO</span>
