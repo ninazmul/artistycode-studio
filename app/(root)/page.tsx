dc392922128a0db30ef0798d2adf3d08ce32b456
ninazmul <nazmulsaw@gmail.com>
2026-04-09 15:55:30 +0600

    Promotion location changed and set between services and approach

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,10 +11,10 @@ const Home = () => {
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <Promotion />
         <RecentProjects />
         <Clients />
         <Services />
+        <Promotion />
         <Approach />
         <Footer />
       </div>
