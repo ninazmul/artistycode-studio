1a610da2428ffd8e396b59acdb0af3ea5b505d23
ninazmul <nazmulsaw@gmail.com>
2026-04-09 21:19:49 +0600

    layout fixed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,23 +3,24 @@
 import { Toaster } from "react-hot-toast";
 import ScrollHeaderWrapper from "@/components/ScrollHeaderWrapper";
 import Header from "@/components/Header";
-import InteractiveBackground from "@/components/InteractiveBackground";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+
   return (
-    <div className="relative flex h-screen flex-col overflow-hidden">
+    <div className="flex h-screen flex-col">
       <Toaster />
       <ScrollHeaderWrapper>
         <Header />
       </ScrollHeaderWrapper>
 
-      <InteractiveBackground />
-
-      <main className="flex-1 relative z-10">{children}</main>
+      <main className="flex-1">
+        {children}
+      </main>
     </div>
   );
 }
